a520a660fbba069f764eca1aa0b7976556eda943
Richard Chien <richardchienthebest@gmail.com>
2019-02-03 19:40:40 +0800

    Update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 veripress theme install richardchien/veripress-theme-light --name light
 
 theme=`python -c "import config; print(config.THEME)"`
-rm -rf ./themes/$theme/templates/custom
-cp -R ./theme-custom ./themes/$theme/templates/custom
+rm -rf ./themes/${theme}/templates/custom
+cp -R ./theme-custom ./themes/${theme}/templates/custom
 
 veripress generate --app-root=/
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ STORAGE_TYPE = 'file'
 THEME = 'light'
 CACHE_TYPE = 'simple'
 MODE = 'mixed'  # mixed|api-only|view-only
-ENTRIES_PER_PAGE = 5
+ENTRIES_PER_PAGE = 10
 FEED_COUNT = 10
 SHOW_TOC = True
 TOC_DEPTH = 3  # 1~6
@@ -11,3 +11,13 @@ ALLOW_SEARCH_PAGES = True
 PAGE_SOURCE_ACCESSIBLE = False
 DISQUS_ENABLED = True
 DISQUS_SHORT_NAME = 'richardchien'
+# WILDFIRE_ENABLED = True
+WILDFIRE_DATABASE_PROVIDER = 'firebase'
+WILDFIRE_DATABASE_CONFIG = """{
+    apiKey: "AIzaSyBndcp51fg2Z5iUCtGEDzfYQirfLhc6PiQ",
+    authDomain: "test-f40f1.firebaseapp.com",
+    databaseURL: "https://test-f40f1.firebaseio.com",
+    projectId: "test-f40f1",
+    storageBucket: "test-f40f1.appspot.com",
+    messagingSenderId: "479751802039"
+}"""
