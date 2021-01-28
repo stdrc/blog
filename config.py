155ecfe8ee8a2ee6a0a20a8a0a20e3097e32b502
Richard Chien <richardchienthebest@gmail.com>
2021-01-28 22:24:36 +0800

    update for purepress 0.2.0

--- config.py
+++
@@ -1,8 +0,0 @@
-SITE_INFO = {
-    "title": "Project RC",
-    "subtitle": "一只腊鸡的技术成长。",
-    "author": "Richard Chien",
-    "timezone": "Asia/Shanghai",
-}
-DISQUS_ENABLED = True
-DISQUS_SHORT_NAME = "richardchien"
